0539ce4ac24645743176da0eb1c95d7e16f0c24a
Isaias Valle <guitartsword@users.noreply.github.com>
2025-11-01 22:59:56 -0600

    configuring posthog

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { PostHogProvider } from "posthog-js/react";
 import {
   isRouteErrorResponse,
   Links,
@@ -42,7 +43,19 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: '2025-05-24',
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+        debug: import.meta.env.MODE === "development",
+      }}
+    >
+      <Outlet />
+    </PostHogProvider>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
